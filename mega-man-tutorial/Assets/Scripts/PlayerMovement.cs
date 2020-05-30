7ddcb536a74671f8cd707ee86f40e6da11c6d89f
williamdsw <williamdsw@outlook.com>
2020-05-30 16:41:08 -0300

    Mega Man Tutorial -> #002 - Player Input

---
+++ mega-man-tutorial/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // FIELDS
+
+    [SerializeField]
+    private float horizontalSpeed = 0;
+
+    [SerializeField]
+    private bool isHoldingJumpButton = false;
+
+
+    // OVERRIDED FUNCTIONS
+
+    private void Start () 
+    {
+        
+    }
+
+    private void Update () 
+    {
+
+    }
+
+    // INPUT ACTIONS FUNCTIONS
+
+    public void OnMovement (InputAction.CallbackContext callback)
+    {
+        horizontalSpeed = callback.ReadValue<float>();
+    }
+
+    public void OnJump (InputAction.CallbackContext callback) 
+    {
+        if (callback.started)
+        {
+            Debug.Log ("Iniciou pulo");
+        }
+
+        isHoldingJumpButton = callback.performed;
+    }
+
+}
