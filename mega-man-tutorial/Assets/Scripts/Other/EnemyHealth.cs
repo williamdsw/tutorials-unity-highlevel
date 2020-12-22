c7b40718c7906357b5e8689f7edb3abb63801cdc
William Santos <williamdsw@outlook.com>
2020-12-22 16:15:36 -0300

    Mega Man Tutorial -> #014 - Enemies (2)

---
+++ mega-man-tutorial/Assets/Scripts/Other/EnemyHealth.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : Damageable
+{
+    public override void Death()
+    {
+        Destroy(this.gameObject);
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+}
