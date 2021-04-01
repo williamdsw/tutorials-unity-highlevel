8d9954e5bb86c273e5986e2f30c0f2848bdb04cf
William Santos <williamdsw@outlook.com>
2021-03-31 21:36:17 -0300

    Updating new-input-system to 2020.3.1f1

--- new-input-system/Assets/Scripts/PlayerMovement.cs
+++ new-input-system/Assets/Scripts/PlayerMovement.cs
@@ -1,59 +1,62 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+namespace NewInputSystem
 {
-    [SerializeField] private float speed = 6f;
-    [SerializeField] private float jumpForce = 10f;
-    [SerializeField] private float horizontal;
-    [SerializeField] private bool isJumping;
-
-    private Rigidbody2D rigidBody2D;
-    private PlayerInputActions input;
-
-    private void Awake()
+    public class PlayerMovement : MonoBehaviour
     {
-        rigidBody2D = this.GetComponent<Rigidbody2D>();
-        input = new PlayerInputActions();
-    }
+        [SerializeField] private float speed = 6f;
+        [SerializeField] private float jumpForce = 10f;
+        [SerializeField] private float horizontal;
+        [SerializeField] private bool isJumping;
 
-    private void Update()
-    {
-        // isJumping = isJumping || input.PlayerActions.Jump.triggered;
-    }
+        private Rigidbody2D rigidBody2D;
+        private PlayerInputActions input;
 
-    private void FixedUpdate()
-    {
-        // horizontal = input.PlayerActions.Movement.ReadValue<float>();
-        rigidBody2D.velocity = new Vector2(horizontal * speed, rigidBody2D.velocity.y);
-
-        if (isJumping)
+        private void Awake()
         {
-            isJumping = false;
-            rigidBody2D.velocity = Vector2.zero;
-            rigidBody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rigidBody2D = this.GetComponent<Rigidbody2D>();
+            input = new PlayerInputActions();
         }
-    }
 
-    // Necessario para habilitar / desabilitar o sistema de input
+        private void Update()
+        {
+            // isJumping = isJumping || input.PlayerActions.Jump.triggered;
+        }
 
-    private void OnEnable()
-    {
-        input.Enable();
-    }
+        private void FixedUpdate()
+        {
+            // horizontal = input.PlayerActions.Movement.ReadValue<float>();
+            rigidBody2D.velocity = new Vector2(horizontal * speed, rigidBody2D.velocity.y);
 
-    private void OnDisable()
-    {
-        input.Disable();
-    }
+            if (isJumping)
+            {
+                isJumping = false;
+                rigidBody2D.velocity = Vector2.zero;
+                rigidBody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+        }
 
-    public void Jump(InputAction.CallbackContext callback)
-    {
-        isJumping = callback.performed;
-    }
+        // Necessario para habilitar / desabilitar o sistema de input
 
-    public void Movement(InputAction.CallbackContext callback)
-    {
-        horizontal = callback.ReadValue<float>();
+        private void OnEnable()
+        {
+            input.Enable();
+        }
+
+        private void OnDisable()
+        {
+            input.Disable();
+        }
+
+        public void Jump(InputAction.CallbackContext callback)
+        {
+            isJumping = callback.performed;
+        }
+
+        public void Movement(InputAction.CallbackContext callback)
+        {
+            horizontal = callback.ReadValue<float>();
+        }
     }
 }
